d9964c72abff1f1813b384244929a1f0bfc69e74
VL <mamiya67@ya.ru>
2019-03-01 21:36:27 +0300

    перехват ракет

--- day_2/gamed2.py
+++ day_2/gamed2.py
@@ -37,8 +37,6 @@ def create_missile(color, x, y, x2, y2):
 
 
 
-
-
 def fire_missile(x, y):
     info = create_missile(color='white', x=BASE_X, y=BASE_Y,
                           x2=x, y2=y)
@@ -71,11 +69,28 @@ def move_missiles(missiles):
                 info['state'] = 'dead'
             else:
                 missile.shapesize(info['radius'])
+        elif state == 'dead':
+            missile.clear()
+            missile.hideturtle()
 
     dead_missiles = [info for info in missiles if info['state'] == 'dead']
     for dead in dead_missiles:
         missiles.remove(dead)
 
+def check_enemy_count():
+    if len(enemy_missiles) < ENEMY_COUNT:
+        fire_enemy_missile()
+
+def check_interceptions():
+    for our_info in our_missiles:
+        if our_info['state'] != 'explode':
+            continue
+        our_missile = our_info['missile']
+        for enemy_info in enemy_missiles:
+            enemy_missile = enemy_info['missile']
+            if enemy_missile.distance(our_missile.xcor(),
+                                      our_missile.ycor()) < 20:
+                enemy_info['state'] = 'dead'
 
 window.onclick(fire_missile)
 
@@ -85,9 +100,7 @@ enemy_missiles = []
 
 while True:
     window.update()
-
-    if len(enemy_missiles) < ENEMY_COUNT:
-        fire_enemy_missile()
-
+    check_enemy_count()
+    check_interceptions()
     move_missiles(missiles=our_missiles)
     move_missiles(missiles=enemy_missiles)
